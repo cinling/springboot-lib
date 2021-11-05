42536b47163885a69a7ed072c88e2d9e2753243c
cinling <1105412681@qq.com>
2021-11-05 10:45:59 +0800

    Init

---
+++ src/test/java/net/cinling/springboot/lib/AppTest.java
@@ -0,0 +1,20 @@
+package net.cinling.springboot.lib;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
